848b1ae5533c2196fc9a64b98a2e882ffda5f011
Antonio Henrique <antoniohenrique1305l@gmail.com>
2025-08-30 16:58:04 -0300

    .

--- src/javagit/QuestCinco.java
+++ src/javagit/QuestCinco.java
@@ -1,11 +1,20 @@
 package javagit;
 import java.util.Scanner;
 
-
-
 public class QuestCinco {
+	public static void inverter(String palavra) {
+		String palavran = "";
+		for(int i = palavra.length()-1; i>=0; i--) {
+			palavran += palavra.charAt(i);
+		}
+		System.out.println(palavran);
+	}
+	
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
+		Scanner palavra = new Scanner(System.in);
+		System.out.println("informe uma string: ");
+		String str = palavra.nextLine();
+		inverter(str);
+		palavra.close();
 	}
 }
